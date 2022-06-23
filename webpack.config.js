8447aa1899312a88e0ef87bb593f89a3f6f5dc5e
anthonyogundipeshr <anthonyogundipe@seamlesshr.com>
2022-06-23 11:52:18 +0100

    update

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+        ]
+    },
+    entry: {
+        main: path.resolve(__dirname, './src/app.js'),
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'deploy')
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Webpack Output",
+        }),
+    ],
+};
+
+
+
